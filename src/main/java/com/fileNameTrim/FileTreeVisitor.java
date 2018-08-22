123bc66c34d9c39a06fb78dca61aee40bf2f9209
Goinmul <21400045@handong.edu>
2018-08-22 10:18:54 +0900

    Creating structure of file system

---
+++ src/main/java/com/fileNameTrim/FileTreeVisitor.java
@@ -0,0 +1,30 @@
+package com.fileNameTrim;
+
+import java.io.File;
+import java.nio.file.Path;
+
+public class FileTreeVisitor {
+
+
+	void fileTraveler(File file)
+	{
+
+		// 1. changing only numbers
+
+
+
+
+
+		// 2. change based on parent folder name
+
+
+
+		//newfile.renameTo();
+
+
+
+	}
+
+
+
+}
--- src/main/java/com/fileNameTrim/FileUtilMain.java
+++ src/main/java/com/fileNameTrim/FileUtilMain.java
@@ -3,15 +3,17 @@ package com.fileNameTrim;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+// input : a header file name( ex - c:/myfile )
+// output : void
+// result : changed names, under all the files in the folders of the header file
 public class FileUtilMain {
 	
 	public static void main (String args[])
 	{
 		Path path = Paths.get("c:/");
-		FileReadingClass fileReader = new FileReadingClass();
-		fileReader.fileTraveler(path);
+		FileTreeVisitor fileReader = new FileTreeVisitor();
 		
-		
+		// get file path from user
 		
 	}
 
